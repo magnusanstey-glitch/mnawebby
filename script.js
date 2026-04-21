3058552a5e0503c68e9c52c5d0517ecb151cb412
aliabbasdjur <aliabbas.0@outlook.com>
2026-04-21 22:57:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ gsap.from(".contact-container", {
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-toggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
